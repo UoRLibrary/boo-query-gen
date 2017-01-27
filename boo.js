241f56c8e7080a90505f4b88c49c663e09111475
px911454 <px911454@reading.ac.uk>
2017-01-27 12:51:10 +0000

    Added: some styling tweaks, descriptive text, and add concept box & clear all functionality

--- boo.js
+++ boo.js
@@ -3,7 +3,7 @@ var search = document.getElementById("search");
 function boo() {
   //Reset search.value
   search.value = "(";
-	
+
   //extract function
   function extract(x) {
     if (/\w/.test(x.value)) {
@@ -14,13 +14,13 @@ function boo() {
       concepts.push(y);
     }
   }
-  
+
   //extract concepts and add to concepts array
-  var concept = document.querySelectorAll('[id^=concept]');
+  var concept = document.querySelectorAll(".concept");
   var concepts = [];
-	for(var i = 0; i < concept.length; i++){
-   extract(concept[i]);
-	}
+  for (var i = 0; i < concept.length; i++) {
+    extract(concept[i]);
+  }
 
   //look for phrases
   for (var i = 0; i < concepts.length; i++) {
@@ -53,9 +53,46 @@ function boo() {
       search.value += combine[i] + ")";
     }
   }
-  
+
   //check for empty search string
-  if (search.value === "(") { 
-  	alert("Please enter some concepts");
-    search.value = ""; }
+  if (search.value === "(") {
+    alert("Please enter some concepts");
+    search.value = "";
+  }
+}
+
+function addConcept() {
+	
+  var cnt = document.getElementsByTagName("div").length;
+  if (cnt < 7) {
+    //find container div  
+    var container = document.getElementById("container");
+
+    //create div
+    var div = document.createElement("div");
+
+    //create h3 and add text
+    var h3 = document.createElement("h3");
+    var count = document.querySelectorAll(".concept");
+    var h3Text = "Concept " + (count.length + 1);
+    h3.appendChild(document.createTextNode(h3Text));
+
+    //create textarea
+    var textarea = document.createElement("textarea");
+    textarea.setAttribute("class", "concept");
+
+    //append new elements
+    container.appendChild(div);
+    div.appendChild(h3);
+    div.appendChild(textarea);
+  } else {
+  	alert("Maximum number of concepts created");
+  }
+} 
+
+function clearAll() {
+	var x = document.getElementsByTagName("textarea");
+  for (var i = 0; i < x.length; i++) {
+  	x[i].value = "";
+  }
 }
