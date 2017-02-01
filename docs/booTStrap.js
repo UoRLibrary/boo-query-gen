de04c4b22ec6e882d5b0e4954d86c7784843f79e
px911454 <px911454@reading.ac.uk>
2017-02-01 10:40:34 +0000

    Test fix issues with loading css

--- docs/booTStrap.js
+++ docs/booTStrap.js
@@ -1,10 +1,11 @@
 ﻿var s0 = document.getElementById("initBooWidget");
+var h0 = document.getElementsByTagName("head");
 
 var wCss = document.createElement("link");
 wCss.setAttribute("rel", "stylesheet");
 wCss.type = "text/css";
 wCss.setAttribute("href", "http://px911454.github.io/boolean-string-generator/boo.css")
-s0.parentNode.insertBefore(wCss, s0);
+h0.appendChild(wCss);
 
 var wBooWidget = document.createElement("script");
 wBooWidget.type = "text/javascript";
