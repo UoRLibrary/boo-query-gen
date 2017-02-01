b92e8c58838c6db89e876d79974dcfbeeac279b4
px911454 <px911454@reading.ac.uk>
2017-02-01 08:56:50 +0000

    Update filepaths

--- widget/booTStrap.js
+++
@@ -1,3 +0,0 @@
-﻿document.write('<link rel="stylesheet" type="text/css" href="https://px911454.github.io/boolean-string-generator/boo.css">');
-document.write('<script type="text/javascript" src="https://px911454.github.io/boolean-string-generator/widget/booWidget.js"></script>');
-document.write('<script type="text/javascript" src="https://px911454.github.io/boolean-string-generator/boo.js"></script>');
