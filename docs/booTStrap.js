6dbdf51766c8e6509b3bda80e1ea5a3870c47800
px911454 <px911454@reading.ac.uk>
2017-02-01 10:42:20 +0000

    Fix link typo

--- docs/booTStrap.js
+++ docs/booTStrap.js
@@ -4,17 +4,17 @@ var h0 = document.getElementsByTagName("head");
 var wCss = document.createElement("link");
 wCss.setAttribute("rel", "stylesheet");
 wCss.type = "text/css";
-wCss.setAttribute("href", "http://px911454.github.io/boolean-string-generator/boo.css")
+wCss.setAttribute("href", "https://px911454.github.io/boolean-string-generator/boo.css")
 h0.appendChild(wCss);
 
 var wBooWidget = document.createElement("script");
 wBooWidget.type = "text/javascript";
 wBooWidget.setAttribute('async', 'async');
-wBooWidget.src = "http://px911454.github.io/boolean-string-generator/docs/booWidget.js";
+wBooWidget.src = "https://px911454.github.io/boolean-string-generator/docs/booWidget.js";
 s0.parentNode.insertBefore(wBooWidget, s0);
 
 var wBooMain = document.createElement("script");
 wBooMain.type = "text/javascript";
 wBooMain.setAttribute('async', 'async');
-wBooMain.src = "http://px911454.github.io/boolean-string-generator/boo.js";
+wBooMain.src = "https://px911454.github.io/boolean-string-generator/boo.js";
 s0.parentNode.insertBefore(wBooMain, s0);
