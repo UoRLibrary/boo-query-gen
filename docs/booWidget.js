efc7a23cefef3078800dc8ddaa005e61c42722fe
px911454 <px911454@reading.ac.uk>
2017-02-01 12:41:37 +0000

    Updated to an AJAX call on booContent.html

--- docs/booWidget.js
+++ docs/booWidget.js
@@ -1,29 +1,8 @@
-﻿var wHTML = "";
-
-wHTML += ('<h3>Search string generator</h3> \
-		<p>\
-		  Use this tool to generate a search string that you can paste into many databases.\
-		  Type your individual keywords/phrases into each concept box, then add synonyms for each concept on separate lines underneath.\
-		  You can use truncation (*) and wildcards (?) to find alternative spellings of those words.\
-		  For example:\
-		</p>\
-		<ul>\
-		  <li><strong>interven*</strong> will find <em>intervene, intervening, intervention</em>, etc.</li>\
-		  <li><strong>colo?r</strong> will find <em>color</em> and <em>colour</em></li>\
-		</ul>\
-		<div id="container">\
-		  <div class="booContainer">\
-			<h4>Concept 1</h4>\
-			<textarea class="concept"></textarea>\
-		  </div>\
-		  <div class="booContainer">\
-			<h4>Concept 2</h4>\
-			<textarea class="concept"></textarea>\
-		  </div>\
-		</div>\
-		<button onclick="boo()">Create search string</button>\
-		<button onclick="clearAll()">Clear all</button>\
-		<button onclick="addConcept()">Add another concept</button>\
-		<h4>Search string</h4>\
-		<textarea id="search"></textarea>');
-document.getElementById('booWidget').innerHTML = wHTML;
+﻿var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    document.getElementById("booWidget").innerHTML = this.responseText;
+  }
+};
+xhttp.open("GET", "https://px911454.github.io/boo-str-gen2/docs/booContent.html", true);
+xhttp.send();
