9b67d12cc07babd46de5b3c3c7e51cdabe9894dc
px911454 <px911454@reading.ac.uk>
2017-02-01 10:26:41 +0000

    Better dynamically loaded script files

--- docs/booTStrap.js
+++ docs/booTStrap.js
@@ -1,3 +1,19 @@
-﻿document.write('<link rel="stylesheet" type="text/css" href="../boo.css">');
-document.write('<script type="text/javascript" src="booWidget.js"></script>');
-document.write('<script type="text/javascript" src="../boo.js"></script>');
+﻿var s0 = document.getElementById("initBooWidget");
+
+var wCss = document.createElement("link");
+wCss.setAttribute("rel", "stylesheet");
+wCss.type = "text/css";
+wCss.setAttribute("href", "../boo.css")
+s0.parentNode.insertBefore(wCss, s0);
+
+var wBooWidget = document.createElement("script");
+wBooWidget.type = "text/javascript";
+wBooWidget.setAttribute('async', 'async');
+wBooWidget.src = "booWidget.js";
+s0.parentNode.insertBefore(wBooWidget, s0);
+
+var wBooMain = document.createElement("script");
+wBooMain.type = "text/javascript";
+wBooMain.setAttribute('async', 'async');
+wBooMain.src = "../boo.js";
+s0.parentNode.insertBefore(wBooMain, s0);
